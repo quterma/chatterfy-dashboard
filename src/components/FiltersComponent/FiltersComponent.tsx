bc33ece93c04ff757c9262a2375462a6940976c6
quterma <dshapiro921@gmail.com>
2024-12-02 12:05:45 +0200

    fix: add minor fixes in styling

--- src/components/FiltersComponent/FiltersComponent.tsx
+++ src/components/FiltersComponent/FiltersComponent.tsx
@@ -1,4 +1,11 @@
-import { useState, useEffect, FC, ChangeEvent } from 'react';
+import {
+  useState,
+  useEffect,
+  useRef,
+  FC,
+  ChangeEvent,
+  useCallback,
+} from 'react';
 import { TYPES, MODELS } from '../../constants/constants';
 import { Filters, Model, Type } from '../../types/types';
 import { useDataService } from '../../context/DataServiceContext';
@@ -24,15 +31,15 @@ export const FiltersComponent: FC = () => {
   };
 
   return (
-    <div className="space-y-6">
-      <SelectField
+    <div className="flex sm:flex-row md:flex-col flex-col gap-5">
+      <CustomSelectField
         id="type"
         label="Type"
         value={type || ''}
         options={[...TYPES]}
         onChange={handleTypeChange}
       />
-      <SelectField
+      <CustomSelectField
         id="model"
         label="Model"
         value={model || ''}
@@ -43,31 +50,80 @@ export const FiltersComponent: FC = () => {
   );
 };
 
-const SelectField: FC<{
+const CustomSelectField: FC<{
   id: string;
   label: string;
   value: string;
   options: string[];
   onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
 }> = ({ id, label, value, options, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+  const closeOptions = () => setIsOpen(false);
+
+  const handleClickOutside = useCallback((event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      closeOptions();
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
   return (
-    <div className="relative">
+    <div ref={ref} className="relative w-full md:w-auto">
       <label htmlFor={id} className="block text-sm font-medium text-gray-700">
         {label}
       </label>
-      <select
-        id={id}
-        value={value}
-        onChange={onChange}
-        className="mt-2 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 over:border-gray-400 cursor-pointer transition-all duration-300 ease-in-out"
+      <div
+        className={`mt-2 w-full p-3 border border-gray-300 rounded-md shadow-sm cursor-pointer focus:outline-none focus:ring-blue-500 focus:border-blue-500 transition-all duration-400 ease-in-out flex justify-between items-center`}
+        onClick={toggleOpen}
       >
-        <option value="">All {label}s</option>
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+        <span>{value || `All ${label}s`}</span>
+        <span
+          className={`transition-transform duration-400 inline-block w-2 h-2 border-t-2 border-r-2 border-gray-500 ${
+            isOpen ? '-rotate-45' : '-rotate-[225deg]'
+          }`}
+        ></span>
+      </div>
+      {isOpen && (
+        <div className="absolute mt-2 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10 transition-all duration-400 ease-in-out">
+          <ul>
+            <li
+              key="all"
+              className="p-2 hover:bg-gray-100 cursor-pointer"
+              onClick={() => {
+                onChange({
+                  target: { value: '' },
+                } as ChangeEvent<HTMLSelectElement>);
+                closeOptions();
+              }}
+            >
+              All {label}s
+            </li>
+            {options.map((option) => (
+              <li
+                key={option}
+                className="p-2 hover:bg-gray-100 cursor-pointer"
+                onClick={() => {
+                  onChange({
+                    target: { value: option },
+                  } as ChangeEvent<HTMLSelectElement>);
+                  closeOptions();
+                }}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/GraphComponent/GraphComponent.tsx
+++ src/components/GraphComponent/GraphComponent.tsx
@@ -29,49 +29,51 @@ export const GraphComponent: FC = () => {
   }, [graphData]);
 
   return (
-    <div className="w-full h-96 p-4 bg-white rounded shadow-md">
-      <h2 className="text-lg font-bold mb-4 text-gray-700">
-        Usage Analytics Over Time
-      </h2>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          data={graphData}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis
-            dataKey="date"
-            tick={{ fontSize: 10 }}
-            label={{
-              value: '',
-              position: 'insideBottomRight',
-              offset: -10,
-            }}
-          />
-          <YAxis
-            tick={{ fontSize: 10 }}
-            domain={yAxisDomain}
-            tickFormatter={(value) => value.toFixed(0)}
-            label={{
-              value: '',
-              angle: -90,
-              position: 'insideLeft',
-            }}
-          />
-          <Tooltip
-            formatter={(value: number) => `${value.toFixed(2)}`}
-            labelFormatter={(label) => `Date: ${label}`}
-          />
-          <Line
-            type="monotone"
-            dataKey="total_cost"
-            stroke="#8884d8"
-            strokeWidth={2}
-            dot={{ r: 3 }}
-            activeDot={{ r: 6 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+    <div className="w-full flex p-4 bg-white rounded shadow-md">
+      <div className="flex-grow h-[30rem] pb-10">
+        <h2 className="text-lg font-bold mb-4 text-gray-700">
+          Usage Analytics Over Time
+        </h2>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={graphData}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="date"
+              tick={{ fontSize: 10 }}
+              label={{
+                value: '',
+                position: 'insideBottomRight',
+                offset: -10,
+              }}
+            />
+            <YAxis
+              tick={{ fontSize: 10 }}
+              domain={yAxisDomain}
+              tickFormatter={(value) => value.toFixed(0)}
+              label={{
+                value: '',
+                angle: -90,
+                position: 'insideLeft',
+              }}
+            />
+            <Tooltip
+              formatter={(value: number) => `${value.toFixed(2)}`}
+              labelFormatter={(label) => `Date: ${label}`}
+            />
+            <Line
+              type="monotone"
+              dataKey="total_cost"
+              stroke="#8884d8"
+              strokeWidth={2}
+              dot={{ r: 3 }}
+              activeDot={{ r: 6 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
